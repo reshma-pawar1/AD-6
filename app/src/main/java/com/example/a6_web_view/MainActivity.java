9933d2dc517286282ed51f0ed0fba794dccf89dc
sgroshani2912 <168357445+reshma-pawar1@users.noreply.github.com>
2024-04-29 08:33:59 +0530

    Initial commit

---
+++ app/src/main/java/com/example/a6_web_view/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.a6_web_view;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+
+        WebView webview = findViewById(R.id.webview1);
+        WebSettings webSettings = webview.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webview.setWebViewClient(new WebViewClient());
+        webview.loadUrl("https://pict.edu/");
+
+    }
+
+    @Override
+    public void onBackPressed(){
+        if(webView.canGoBack()) webView.goBack();
+        else super.onBackPressed();
+    }
+}
